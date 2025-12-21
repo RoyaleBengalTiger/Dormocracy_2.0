b56d56505210977179fd2f33fb90abaa4226b1ba
RoyaleBengalTiger <siamsifat244@gmail.com>
2025-12-21 14:18:37 +0600

    database schema & authentication implemented

---
+++ backend/src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
+
+import { DepartmentsModule } from './departments/departments.module';
+import { RoomsModule } from './rooms/rooms.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { TasksModule } from './tasks/tasks.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+
+    DepartmentsModule,
+    RoomsModule,
+    UsersModule,
+
+    AuthModule,
+
+    TasksModule,
+  ],
+})
+export class AppModule { }
